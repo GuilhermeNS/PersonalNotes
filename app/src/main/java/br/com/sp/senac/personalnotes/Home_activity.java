58433b978afbc2e124b68cbdfe80e3c375a751ef
guilherme.nsantos7 <guilherme_nsantos2002@hotmail.com>
2020-03-05 11:55:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/personalnotes/Home_activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.personalnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+    }
+}
---
+++ app/src/main/java/br/com/sp/senac/personalnotes/Notes_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.personalnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notes_layout);
+    }
+}
